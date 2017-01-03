be71956db095693f635e597dbebac01fea8be8ce
JonathanEleiott <jonathan.eleiott@gmail.com>
2017-01-02 21:57:18 -0800

    Newest messages show up

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,7 +19,6 @@ $(document).ready(function() {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        app.fetch();
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
@@ -39,6 +38,7 @@ $(document).ready(function() {
       contentType: 'application/json',
       data: 'order=-createdAt',
       success: function (data) {
+        app.clearMessages();
         console.log('get data', data);
         for (var resultsLength = data.results.length - 1; resultsLength > data.results.length - 101; resultsLength--) {
           $('#chats').append('<div>' + data.results[resultsLength].username + ' : ' + data.results[resultsLength].text + '</div>');
@@ -75,13 +75,9 @@ $(document).ready(function() {
 
   };
 
-  // var message = {
-  //   username: 'Mel Brooks',
-  //   text: 'It\'s good to be the king',
-  //   roomname: 'lobby'
-  // };
-
-  // app.renderMessage(message);
+  setInterval(function() {
+    app.fetch();
+  }, 5000);
 
 
 });
