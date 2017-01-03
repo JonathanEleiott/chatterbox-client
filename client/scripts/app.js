edefd863f83e5d9f23c11032f7735e522d5f768d
JonathanEleiott <jonathan.eleiott@gmail.com>
2017-01-02 16:43:18 -0800

    Add new messages every second

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -23,29 +23,37 @@
 
 
 
-var getMessages = function() {
-
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/messages',
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      for (var i = 0; i < data.results.length; i++) {
-        $('#messages').append('<div>' + data.results[i].username + ' : ' + data.results[i].text + '</div>');
-      }      
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('failed to retrieve', data);
-    }
-  });
-};
 
 $(document).ready(function() {
 
+  var resultsLength = 0;
+
   setInterval(function() {
-    $('#messages').append(getMessages());
+    console.log('here');
+    $('#messages').append(getMessages(resultsLength));
   }, 1000);
+
+
+  var getMessages = function() {
+
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(resultsLength);
+        for (; resultsLength < data.results.length; resultsLength++) {
+          $('#messages').append('<div>' + data.results[resultsLength].username + ' : ' + data.results[resultsLength].text + '</div>');
+        }      
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('failed to retrieve', data);
+      }
+    });
+  };
+
+
 
 });
